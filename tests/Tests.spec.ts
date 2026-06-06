21935f885901deabe16e9ececb05f8e5bf4f7076
Mohamed <mohamed.abdelhakim.ahmed2356@gmail.com>
2026-06-06 19:33:08 +0300

    removed white spaces

--- tests/Tests.spec.ts
+++ tests/Tests.spec.ts
@@ -1,13 +1,13 @@
 import { test } from "../fixtures/TestFixtures";
 import { test as test2 } from "../fixtures/TestFixturesNoLogin";
 import { Links } from "../pages/NavigationBar";
-import {expect} from "@playwright/test";
+import { expect } from "@playwright/test";
 import fs from 'fs';
 import path from 'path';
 import { parse } from "csv-parse/sync";
 import { generateFakeName, generateFakeEmail } from "../utilities/GenerateFakeData";
-import {captureScreenshot} from "../utilities/Screenshots";
-import {AxeBuilder} from "@axe-core/playwright";
+import { captureScreenshot } from "../utilities/Screenshots";
+import { AxeBuilder } from "@axe-core/playwright";
 
 type ProductRecord = { name: string; price: string };
 
@@ -18,20 +18,15 @@ const csvPath = path.join(
 const productData = parse<ProductRecord>(fs.readFileSync(csvPath, 'utf-8'), { columns: true, skip_empty_lines: true });
 
 test.describe("Sign Up Tests", () => {
-    test2("test new features", async ({ homePage, navigationBar, signUpLoginPage, signUpPage,currentTestInfo ,accessibility}) => {
+    test2("test new features", async ({ homePage, navigationBar, signUpLoginPage, signUpPage, currentTestInfo, accessibility }) => {
         console.log(`Hello ${process.env.HELLO}`)
-
-        await navigationBar.clickOnLink(Links.SIGNUP_IN,currentTestInfo);
-
-    const results = await accessibility.analyze();
-
-
-
-        await captureScreenshot( homePage.page,"signup_login",currentTestInfo);
+        await navigationBar.clickOnLink(Links.SIGNUP_IN, currentTestInfo);
+        const results = await accessibility.withTags(["wcag2a", "wcag2aa"]).analyze();
+        await captureScreenshot(homePage.page, "signup_login", currentTestInfo);
         await signUpLoginPage.enterUsername("testuser");
         await signUpLoginPage.enterEmail("testuser01036@example.com");
         await signUpLoginPage.clickSignUpButton();
-            expect(results.violations).toEqual([]);
+        expect(results.violations).toEqual([]);
         await signUpPage.selectGender("male");
         await signUpPage.enterName("Test User");
         await signUpPage.enterPassword("password123");
@@ -57,7 +52,7 @@ test.describe("Sign Up Tests", () => {
 });
 
 test.describe("login", () => {
-    test("Add product to cart", async ({ homePage, navigationBar, productsPage, cartPage, checkoutPage, cardDetails, placedOrder,currentTestInfo }) => {
+    test("Add product to cart", async ({ homePage, navigationBar, productsPage, cartPage, checkoutPage, cardDetails, placedOrder, currentTestInfo }) => {
 
         await homePage.page.route("**/add_to_cart/1", async route => {
             // Only intercept POST requests to the API endpoint
@@ -150,8 +145,8 @@ test.describe("params", () => {
         { name: 'Sleeveless Dress', price: 'Rs. 1000' },
         { name: 'Blue Top', price: 'Rs. 500' },
     ].forEach(({ name, price }) => {
-        test.skip(`test ${name}`, async ({ homePage, navigationBar, productsPage, cartPage, checkoutPage, cardDetails, placedOrder,currentTestInfo }) => {
-            await navigationBar.clickOnLink(Links.PRODUCTS,currentTestInfo);
+        test.skip(`test ${name}`, async ({ homePage, navigationBar, productsPage, cartPage, checkoutPage, cardDetails, placedOrder, currentTestInfo }) => {
+            await navigationBar.clickOnLink(Links.PRODUCTS, currentTestInfo);
             await productsPage.typeInSearchButton(name);
             await productsPage.clickOnSearchButton();
             await productsPage.hoverAddToCartButton(name, 1);
@@ -270,5 +265,5 @@ test.describe("tetss", () => {
         console.log(`Hello ${generateFakeName()}`)
         console.log(`Hello ${generateFakeEmail()}`)
     });
-});                                                                                               
+});
 
